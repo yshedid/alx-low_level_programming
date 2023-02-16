adb503b89f7844f794e634789c6332403e34d25a
yshedid <yosef.shedid.prof@gmail.com>
2023-02-16 20:58:23 +0000

    hello printf c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - the program entry
+ * Return:exits the program
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
--- 0x00-hello_world/test.c
+++ 0x00-hello_world/test.c
@@ -1,1 +1,10 @@
-test.o
+#include <stdio.h>
+/**
+ *main - entry point to my program that prints hello world
+ *Return: exits the program
+ */
+int main(void)
+{
+printf("hello world in c! \n");/* printing hello world*/
+return (0);
+}
