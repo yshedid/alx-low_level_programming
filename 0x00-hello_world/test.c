c5adf5e8d59424a67486770fe299d5ca843aee69
yshedid <yosef.shedid.prof@gmail.com>
2023-02-16 20:30:55 +0000

    compiling c without linking

--- 0x00-hello_world/test.c
+++ 0x00-hello_world/test.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-/**
- *main - entry point to my program that prints hello world
- *Return: exits the program
- */
-int main(void)
-{
-printf("hello world in c! \n");/* printing hello world*/
-return (0);
-}
+test.o
