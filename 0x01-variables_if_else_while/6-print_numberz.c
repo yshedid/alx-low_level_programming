5fea9bd6493fc28ae15be9dbc3d03a12a1a937e4
yshedid <yosef.shedid.prof@gmail.com>
2023-02-19 17:57:28 +0000

    printing numbers from 0 to 10 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point to the program and contains all the code
+ * Return: exits the program
+ */
+int main(void)
+{
+int x;
+x = 48;
+while (x <= 57)
+{
+	putchar(x);
+	x++;
+}
+putchar('\n');
+return (0);
+}
