82769658ae3576775371a05a2bf9e4d1371ba4b9
yshedid <yosef.shedid.prof@gmail.com>
2023-02-19 14:55:23 +0000

    first project in this directory about if

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ *main - the entry point in the program (the whole program now)
+ * Return: exits the program
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%d is positive\n", n);
+else if (n < 0)
+	printf("%d is negative\n", n);
+else
+	printf("%d is zero\n", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+more projects on c using if statments and while and more advanced stuff
