019f96544c0ebf18702f78cae0a4d066108bf148
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 14:47:41 +0000

    c file contins _islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * _islower - checks if the charcter in lower case
+ * Return: 1 if c is lowercase
+ * 0 otherwise
+ */
+int _islower(int c)
+{
+if (c > 96)
+	return (1);
+else
+	return (0);
+}
