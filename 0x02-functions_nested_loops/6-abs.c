8efe728e0ef645fb4a66566b78718b71c97e73cd
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 15:39:20 +0000

    c file with _abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - calculates the absolute
+ * @n: number to get it's abs
+ * Return: returns the abs of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
