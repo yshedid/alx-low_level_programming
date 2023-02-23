3e94cde8111b7317800d1fe5aef3a5fd7a9a6081
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 14:25:17 +0000

    function prints alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet_x10 - prints the alphabet in lower case
+ */
+void print_alphabet_x10(void)
+{
+int counter;
+counter = 10
+while (counter > 0)
+{
+char i;
+i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+_putchar('\n');
+counter--;
+}
+}
