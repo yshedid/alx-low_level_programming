bfe434214942b154fa3a651e6da343e81475c088
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 13:29:11 +0000

    isolated the function in one file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,4 @@
 #include "main.h"
-void print_alphabet(void);
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 /**
  * print_alphabet - prints the alphabet in lower case
  */
