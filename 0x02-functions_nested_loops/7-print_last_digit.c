478cf3da588d47269ac38d82d3307c5c342d7b3b
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 18:20:28 +0000

    c file contains a function returns the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - it's really obvious from the name
+ * @n:the number that we will get the last digit from
+ *
+ * Return: returns the last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
