20f14910e1b5e9f40b28d99e3619247c04ca1c63
yshedid <yosef.shedid.prof@gmail.com>
2023-02-23 13:11:20 +0000

    created a header file with _putchar function and used them in a project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point the the program
+ * Return: exits the program
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+nested loops, functions, header files projects
