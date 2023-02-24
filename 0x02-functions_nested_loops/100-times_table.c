a2decdc3c38db992d41182f36292ee9a206505d0
yshedid <yosef.shedid.prof@gmail.com>
2023-02-24 13:00:40 +0000

    making progress with that logici for real2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -43,9 +43,9 @@ void print_times_table(int n)
 				break;
 			_putchar(',');
 			_putchar(' ');
+			temp += i;
 			if (temp < 100)
 				_putchar(' ');
-			temp += i;
 			if (temp < 10)
 				_putchar(' ');
 			j++;
