8f2639ba27853748ad1b0232eb696a79d27be2fa
yshedid <yosef.shedid.prof@gmail.com>
2023-03-10 11:44:12 +0000

    first program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - the entry to the program
+ * @argc: the number of arguments
+ * @argv: the arguments
+ *
+ * return: zero indicates success i guess
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Applications to better understanding of argc and argv in C.
