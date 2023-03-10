6c6d69dafe9d861f8c3fb85f3b57dc3ddabe17ea
yshedid <yosef.shedid.prof@gmail.com>
2023-03-10 12:34:03 +0000

    fourth task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point to the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: zero if success 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Erorr\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
