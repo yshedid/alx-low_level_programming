76265ed977a4832f44e5d59d1938e1af6563e677
yshedid <yosef.shedid.prof@gmail.com>
2023-03-15 11:48:20 +0000

    updated 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,10 +10,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	int i;
 
 	if (size == 0)
 		return (NULL);
 	ptr = malloc(size * sizeof(char));
-	ptr[0] = c;
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
 	return (ptr);
 }
