ab2afc5a354e6b168c4b046fa8ceebb307765103
yshedid <yosef.shedid.prof@gmail.com>
2023-03-22 19:03:17 +0000

    second task

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,19 @@
-test
+#include <stdio.h>
+/**
+ * array_iterator - applay a given function to each element of
+ *			the given array
+ * @array: the given array
+ * @size: it's size
+ * @action: the function that will be applied
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
