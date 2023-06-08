5604adbafa9c72948976c6308683c3ed52b79fc1
yshedid <yosef.shedid.prof@gmail.com>
2023-06-08 05:50:11 +0000

    second task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints the reverse of a string
+ * @s: the string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursive(s + 1);
+		_putchar(*s);
+	}
+}
