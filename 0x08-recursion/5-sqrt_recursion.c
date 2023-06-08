127b3eb36c24112d7351742d98d02a41daf14074
yshedid <yosef.shedid.prof@gmail.com>
2023-06-08 07:09:56 +0000

    task numper 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,26 @@
 #include "main.h"
 /**
- * _sqrt_recursion - calculates the a
+ * _sqrt_recursion - calculates the natural square root
+ * @n: the numper
+ *
+ * Return: the square root or -1 if there is no natural sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	sqrt_helper(1, n);
+}
+/**
+ * sqrt_helper - helper function for _sqrt_recusrion
+ * @x: holds a numper to be tested if root or not
+ * @n: x is supposedly the root of n
+ *
+ * Return: the root or -1 if not found
+ */
+int sqrt_helper(int x,int n)
+{
+	if (x > n /2 )
+		return (-1);
+	if (x * x == n)
+		return (x);
+	sqrt_helper(x + 1, n);
+}
