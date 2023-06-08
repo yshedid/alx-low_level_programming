ff913d60c9ec898ed904ab3178d2f868f8859fe2
yshedid <yosef.shedid.prof@gmail.com>
2023-06-08 06:47:57 +0000

    fourth task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * factorial - calculates factorial of a given numper
+ * @n:  the numper
+ *
+ * Return: the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
