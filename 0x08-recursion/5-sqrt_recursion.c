19df927afb8cc7fe1f039ab4cbd08b5dff3706ae
yshedid <yosef.shedid.prof@gmail.com>
2023-10-25 16:56:17 +0000

    sixth task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - calculates the square root
+ * @n: int
+ *
+ * Return: the root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_helper(n, 0));
+}
+
+/**
+ * _sqrt_helper - a helper function for _sqrt_recursion
+ * @n: int
+ * @x: int
+ *
+ * Return: the root
+ */
+
+int _sqrt_helper(int n, int x)
+{
+	if (x <= (n / 2) + 1)
+	{
+		if (x * x == n)
+		{
+			return (x);
+		}
+		else
+			_sqrt_helper(n, x + 1);
+	}
+	return (-1);
+
+}
