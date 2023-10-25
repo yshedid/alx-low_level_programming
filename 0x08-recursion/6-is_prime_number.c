5869124565850a522ab4d8cb0f0665b98edc55db
yshedid <yosef.shedid.prof@gmail.com>
2023-10-25 19:20:46 +0000

    the seventh task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - checks if the number prime or not
+ * @n: the number
+ * Return: 1 if prime 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (is_prime_helper(n, 2));
+}
+/**
+ * is_prime_helper - a helper function for is_prime_number
+ * @n: the number
+ * @c: counter
+ *
+ * Return: 1 if prime 0 otherwise
+ */
+int is_prime_helper(int n, int c)
+{
+	if (n % c)
+		return (0);
+	if (t < n / 2)
+		return (is_prime_helper(n, c + 1));
+	return (1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int, int);
+int is_prime_number(int);
+int is_prime_helper(int, int);
