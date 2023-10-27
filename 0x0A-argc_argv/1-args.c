47e849dd0836610663d0f1e2e88e6a6cf980736d
yshedid <yosef.shedid.prof@gmail.com>
2023-10-27 14:25:14 +0000

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: counter
+ * @argv: the arguments passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
