06961bb33b8f541ea49a1f0b11b75cd8dc8a54fd
yshedid <yosef.shedid.prof@gmail.com>
2023-10-27 14:03:42 +0000

    dealed with a warning

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 
 /**
  * main - entry point
- * @argc: argument counter
+ * @argc: the counter
  * @argv: the argument array
  *
- * Return: always 0
+ * Return: always 1
  */
 int main(int argc,char **argv)
 {
 	printf("%s\n", argv[0]);
-	return (0);
+	return (argc);
 
 }
