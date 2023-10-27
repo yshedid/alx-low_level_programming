fd323950196200a1295f87e431897cbea105fd30
yshedid <yosef.shedid.prof@gmail.com>
2023-10-27 12:54:01 +0000

    h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,11 +1,11 @@
-int _putchar(char c);
+int _putchar(char );
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *s);
+void _puts(char *);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
