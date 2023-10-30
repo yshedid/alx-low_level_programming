c519120a4c8b7e415cb889dfafc26641e06d4ca0
yshedid <yosef.shedid.prof@gmail.com>
2023-10-30 20:17:55 +0000

    edit to the second task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -34,7 +34,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	size = array_size(str);
-	ptr = malloc((sizeof(char) * size) - 1);
+	ptr = malloc((sizeof(char) * size) + 1);
 	if (ptr == NULL)
 		return (NULL);
 
