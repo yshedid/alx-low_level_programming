6c6d17b3aa8d8f66f480c7bacd177d7db637bc1e
yshedid <yosef.shedid.prof@gmail.com>
2023-11-01 02:40:05 +0000

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,105 +1,41 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
- * array_size - returns the size of an array
- * @s: the string/array
+ * argstostr - concatenates two strings
+ * @ac: number of arguments
+ * @av: arguments(strings)
  *
- * Return: size
+ * Return: the concatenated string
  */
-int array_size(char *s)
-{
-        int count;
-
-        count = 0;
-        if (s == NULL)
-                return (count);
-        while (*s != '\0')
-        {
-                count++;
-                s += 1;
-        }
-        return (count);
-}
-/**
- * str_concat - concatenats two strings
-* @s1: first string
- * @s2: second string
- *
- * Return: the new string
- */
-char *str_concat(char *s1, char *s2, int last,int size_1,int size_2)
-{
-        int new_size;
-        int counter;
-        char *ptr;
-
-        new_size = size_1 + size_2;
-        if (new_size == 0)
-        {
-                ptr = malloc(1);
-                ptr[0] = '\0';
-                return (ptr);
-        }
-	if (last == 0)
-		ptr = malloc(new_size + 1);
-	else
-		ptr = malloc(new_size + 1);
-        if (ptr)
-        {
-                counter = 0;
-                size_2 = 0;
-                while (counter < new_size)
-                {
-                        if (counter < size_1)
-                        {
-                                ptr[counter] = s1[counter];
-                        }
-                        else
-                        {
-                                ptr[counter] = s2[size_2];
-                                size_2++;
-                        }
-                        counter++;
-                }
-		ptr[counter] = '\n';
-		if (last == 1)
-		{
-			ptr[counter + 1] ='\0';
-		}
-
-                return (ptr);
-        }
-        return (ptr);
-}
 char *argstostr(int ac, char **av)
 {
 	char *ptr;
-	char *temp;
-	int count;
-	int size_1;
-	int size_2;
+	size_t i, j, size_sum;
 
-	ptr = NULL;
-	count = 0;
-	size_1 = 0;
-	while (count < ac)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	i = size_sum = 0;
+	while (i < (size_t)ac)
 	{
-		
-		size_2 = array_size(av[count]) ;
-		if (count + 1 == ac)
-		{
-			temp = str_concat(ptr, av[count], 1, size_1, size_2);
-			free(ptr);
-			ptr = temp;
-		}
-		else
-		{	temp = str_concat(ptr, av[count], 0, size_1, size_2);
-			free(ptr);
-			ptr = temp;
-
-		}
-		size_1 += size_2 + 1;
-		count++;
+		size_sum += 1 + strlen(av[i]);
+		i++;
 	}
+	ptr = malloc(size_sum + 1);
+	i = 0;
+	size_sum = 0;
+	while (i < (size_t)ac)
+	{
+		j = 0;
+		while (j < strlen(av[i]))
+		{
+			ptr[j + size_sum] = av[i][j];
+			j++;
+		}
+		ptr[j + size_sum] = '\n';
+		size_sum += strlen(av[i]) + 1;
+		i++;
+	}
+	ptr[size_sum] = '\0';
 	return (ptr);
-
 }
---
+++ 0x0B-malloc_free/test.c
@@ -0,0 +1,117 @@
+#include <stdlib.h>
+#include <stdio.h>
+char *argstostr(int ac, char **av);
+/**
+ * array_size - returns the size of an array
+ * @s: the string/array
+ *
+ * Return: size
+ */
+int main(int ac, char *av[])
+{
+    char *s;
+
+    s = argstostr(ac, av);
+    if (s == NULL)
+    {
+        return (1);
+    }
+    printf("%s", s);
+    free(s);
+    return (0);
+}
+int array_size(char *s)
+{
+        int count;
+
+        count = 0;
+        if (s == NULL)
+                return (count);
+        while (*s != '\0')
+        {
+                count++;
+                s += 1;
+        }
+        return (count);
+}
+/**
+ * str_concat - concatenats two strings
+* @s1: first string
+ * @s2: second string
+ *
+ * Return: the new string
+ */
+char *str_concat(char *s1, char *s2, int last,int size_1,int size_2)
+{
+        int new_size;
+        int counter;
+        char *ptr;
+
+        new_size = size_1 + size_2;
+        if (new_size == 0)
+        {
+                ptr = malloc(1);
+                ptr[0] = '\0';
+                return (ptr);
+        }
+		printf("new size is %d\n",new_size);
+                ptr = malloc(new_size * sizeof(char) + 2);
+        if (ptr)
+        {
+                counter = 0;
+                size_2 = 0;
+                while (counter < new_size)
+                {
+                        if (counter < size_1)
+                        {
+                                ptr[counter] = s1[counter];
+                        }
+                        else
+                        {
+                                ptr[counter] = s2[size_2];
+                                size_2++;
+                        }
+                        counter++;
+                }
+                ptr[counter] = '\n';
+                if (last == 1)
+                {
+                        ptr[counter + 1] ='\0';
+                }
+
+                return (ptr);
+        }
+        return (ptr);
+}
+char *argstostr(int ac, char **av)
+{
+        char *ptr;
+        char *temp;
+        int count;
+        int size_1;
+        int size_2;
+
+        ptr = NULL;
+        count = 0;
+        size_1 = 0;
+        while (count < ac)
+        {
+
+                size_2 = array_size(av[count]) ;
+                if (count + 1 == ac)
+                {
+                        temp = str_concat(ptr, av[count], 1, size_1, size_2);
+                        free(ptr);
+                        ptr = temp;
+                }
+                else
+                {       temp = str_concat(ptr, av[count], 0, size_1, size_2);
+                        free(ptr);;
+                        ptr = temp;
+                }
+                size_1 += size_2 + 1;
+                count++;
+        }
+        return (ptr);
+
+}
