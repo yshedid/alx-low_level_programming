d851bf220c82059aa74a08b3900f3ed48e6c3712
yshedid <yosef.shedid.prof@gmail.com>
2023-11-09 02:26:32 +0000

    third task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ * int_index - search algorthim
+ * @array: array
+ * @size: it's size
+ * @cmp: comparing function
+ *
+ * Return: index if success -1 otherwise
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
